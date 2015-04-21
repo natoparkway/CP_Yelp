3836a2cf141fe73c1c5fdc3a125c4db94db5689b
Nathaniel Okun <nokun@stanford.edu>
2015-04-20 22:34:02 -0700

    Finishing Up

---
+++ Yelp/Business.swift
@@ -0,0 +1,13 @@
+//
+//  Business.swift
+//  Yelp
+//
+//  Created by Nathaniel Okun on 4/16/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class Business: NSObject {
+   
+}
---
+++ Yelp/GeneralFilterCell.swift
@@ -0,0 +1,24 @@
+//
+//  GeneralFilterCell.swift
+//  Yelp
+//
+//  Created by Nathaniel Okun on 4/19/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class GeneralFilterCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
